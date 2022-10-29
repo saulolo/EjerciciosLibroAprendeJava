cb1b1631462ef70cbd53da047def1236cd91873e
Saul Echeverri <111350593+saulolo@users.noreply.github.com>
2022-10-28 20:07:06 -0500

    Version 1. Ejercicios del Libro de Aprende Java. 28-oct-2022

---
+++ src/Unit4_Condicionales/Ejercicio4_5_25.java
@@ -0,0 +1,41 @@
+package Unit4_Condicionales;
+
+public class Ejercicio4_5_25 {
+    
+    public static void main(String[] args) {
+        
+        /*La tienda online BanderaDeEspaña.es vende banderas personalizadas de 
+        la máxima calidad y nos ha pedido hacer un configurador que calcule el 
+        precio según el alto y el ancho. El precio base de una bandera es de un 
+        céntimo de euro el centímetro cuadrado. Si la queremos con un escudo 
+        bordado, el precio se incrementa en 2.50 € independientemente del tamaño. 
+        Los gastos de envío son 3.25 €. El IVA ya está incluido en todas las 
+        tarifas.
+        
+        Ejemplo 1:
+        Introduzca la altura de la bandera en cm: 20
+        Ahora introduzca la anchura: 35
+        ¿Quiere escudo bordado? (s/n): n
+        Gracias. Aquí tiene el desglose de su compra.
+        Bandera de 700 cm2: 7,00 €
+        Sin escudo: 0,00 €
+        Gastos de envío: 3,25 €
+        Total: 10,25 €
+        
+        Ejemplo 2:
+        Introduzca la altura de la bandera en cm: 10
+        Ahora introduzca la anchura: 15
+        ¿Quiere escudo bordado? (s/n): s
+        Gracias. Aquí tiene el desglose de su compra.
+        Bandera de 150 cm2: 1,50 €
+        Con escudo: 2,50 €
+        Gastos de envío: 3,25 €
+        Total: 7,25 €
+        */
+        
+        System.out.println("TIENDA ONLINE BANDERA DE ESPAÑA");
+        System.out.println("-------------------------------");
+
+    }
+    
+}
