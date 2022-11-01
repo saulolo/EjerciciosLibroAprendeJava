ed421809bb35c0dc0399a5b250ce882a80305ae2
Saul Echeverri <111350593+saulolo@users.noreply.github.com>
2022-11-01 08:14:18 -0500

    iNICIANDO EJERCICIO 4_4_27. 01-11-22

---
+++ src/Unit4_Condicionales/Ejercicio4_5_27.java
@@ -0,0 +1,49 @@
+package Unit4_Condicionales;
+
+/* @author: Saulolo */
+
+public class Ejercicio4_5_27 {
+
+    public static void main(String[] args) {
+        
+        
+        /*Una pastelería nos ha pedido realizar un programa que haga 
+        presupuestos de tartas. El programa preguntará primero de qué sabor 
+        quiere el usuario la tarta: manzana, fresa o chocolate. La tarta de 
+        manzana vale 18 euros y la de fresa 16. En caso de seleccionar la tarta 
+        de chocolate, el programa debe preguntar además si el chocolate es negro
+        o blanco; la primera opción vale 14 euros y la segunda 15. Por último 
+        se pregunta si se añade nata y si se personaliza con un nombre; la nata 
+        suma 2.50 y la escritura del nombre 2.75.
+        
+        Ejemplo 1:
+        Elija un sabor (manzana, fresa o chocolate): chocolate
+        ¿Qué tipo de chocolate quiere? (negro o blanco): negro
+        ¿Quiere nata? (si o no): si
+        ¿Quiere ponerle un nombre? (si o no): no
+        Tarta de chocolate negro: 14,00 €
+        Con nata: 2,50 €
+        Total: 16,50 €
+        
+        Ejemplo 2:
+        Elija un sabor (manzana, fresa o chocolate): manzana
+        ¿Quiere nata? (si o no): no
+        ¿Quiere ponerle un nombre? (si o no): si
+        Tarta de manzana: 18,00 €
+        Con nombre: 2,75 €
+        Total: 20,75 €
+        
+        Ejemplo 3:
+        Elija un sabor (manzana, fresa o chocolate): fresa
+        ¿Quiere nata? (si o no): si
+        ¿Quiere ponerle un nombre? (si o no): si
+        Tarta de fresa: 16,00 €
+        Con nata: 2,50 €
+        Con nombre: 2,75 €
+        Total: 21,25 €*/
+        
+        System.out.println("PRESUPUESTOS DE TARTAS");
+        System.out.println("----------------------");
+        
+    }
+}
