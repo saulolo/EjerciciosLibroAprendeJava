4e433c3a6282363e74d973b1789a43dc31748ad1
Saul Echeverri <111350593+saulolo@users.noreply.github.com>
2022-11-03 10:27:12 -0500

    Voy para el ejercicio 4_5_29 02-11-22

--- src/Unit4_Condicionales/Ejercicio4_5_28.java
+++ src/Unit4_Condicionales/Ejercicio4_5_28.java
@@ -1,10 +1,14 @@
 package Unit4_Condicionales;
 
+import java.util.Scanner;
+
 /* @author: Saulolo */
 
 public class Ejercicio4_5_28 {
 
     public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
         
         /*Implementa el juego piedra, papel y tijera. Primero, el usuario 1 
         introduce su jugada y luego el usuario 2. Si alguno de los usuarios 
@@ -27,7 +31,55 @@ public class Ejercicio4_5_28 {
         Gana el jugador 1*/
         
         System.out.println("JUEGO: PIEDRA, PAPEL Y TIJERAS");
-        System.out.println("------------------------------");            
+        System.out.println("------------------------------");        
+        
+        
+        String jugador;
+        String jugador2;
+        
+        System.out.println("Turno del jugador 1 (introduzca piedra, papel o tijera): ");
+        jugador = input.nextLine();
+        
+        System.out.println("Turno del jugador 2 (introduzca piedra, papel o tijera): ");
+        jugador2 = input.nextLine();
+        
+        switch (jugador) {
+            case "papel":
+                if (jugador2.equals("papel")) {
+                    System.out.println("Empate");
+                } 
+                else if (jugador2.equals("tijera")){
+                    System.out.println("Gana el jugador 2");
+                }    
+                else if (jugador2.equals("piedra")){
+                    System.out.println("Gana el jugador 1");
+                }
+                break;
+            case "tijera":
+                if (jugador2.equals("papel")) {
+                    System.out.println("Gana el jugador 1");
+                } 
+                else if (jugador2.equals("piedra")){
+                    System.out.println("Gana el jugador 2");
+                }    
+                else if (jugador2.equals("tijera")){
+                    System.out.println("Empate");
+                }
+                break;
+            case "piedra":
+                if (jugador2.equals("papel")) {
+                    System.out.println("Gana el jugador 2");
+                } 
+                else if (jugador2.equals("tijera")){
+                    System.out.println("Gana el jugador 1");
+                }    
+                else if (jugador2.equals("piedra")){
+                    System.out.println("Empate");
+                }
+                break;
+            default:
+                System.out.println("Error");
+        }
         
     }
 }
---
+++ src/Unit4_Condicionales/Ejercicio4_5_29.java
@@ -0,0 +1,37 @@
+package Unit4_Condicionales;
+
+/* @author: Saulolo */
+
+public class Ejercicio4_5_29 {
+
+    public static void main(String[] args) {
+        
+        /*Realiza un programa que calcule el precio de un desayuno. El programa 
+        preguntará primero qué ha tomado el usuario de comer: palmera, donut o
+        pitufo. La palmera vale 1.40 € y el donut 1 €. En caso de tomar pitufo, 
+        el programa debe preguntar además si era con aceite o con tortilla; el 
+        primero vale 1’20 € y el segundo 1’60 €. Por último se pregunta por la 
+        bebida: zumo o café a 1’50 y 1’20 respectivamente.
+        
+        Ejemplo 1:
+        ¿Qué ha tomado de comer? (palmera, donut o pitufo): palmera
+        ¿Qué ha tomado de beber? (zumo o café): café
+        Palmera: 1,40 €
+        Café: 1,20 €
+        Total desayuno: 2,60 €
+        
+        Ejemplo 2:
+        ¿Qué ha tomado de comer? (palmera, donut o pitufo): pitufo
+        ¿Con qué se ha tomado el pitufo? (aceite o tortilla): tortilla
+        ¿Qué ha tomado de beber? (zumo o café): zumo
+        Pitufo con tortilla: 1,60 €
+        Zumo: 1,50 €
+        Total desayuno: 3,10 €
+        */
+        
+        System.out.println("PRECIO DEL DESAYUNO");
+        System.out.println("-------------------");
+        
+    }
+    
+}
