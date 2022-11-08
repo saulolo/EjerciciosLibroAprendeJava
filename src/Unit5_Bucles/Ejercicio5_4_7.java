de699bf43b46138362c235d75aced3d1d4b3ff1a
Saul Echeverri <111350593+saulolo@users.noreply.github.com>
2022-11-08 11:49:06 -0500

    Voy para Unidad ejercicio 7. 8-11-22

---
+++ src/Unit5_Bucles/Ejercicio5_4_7.java
@@ -0,0 +1,19 @@
+package Unit5_Bucles;
+
+/* @author: Saulolo */
+
+public class Ejercicio5_4_7 {
+
+    public static void main(String[] args) {
+        
+        /*Realiza el control de acceso a una caja fuerte. La combinación será un
+        número de 4 cifras. El programa nos pedirá la combinación para abrirla. 
+        Si no acertamos, se nos mostrará el mensaje “Lo siento, esa no es la 
+        combinación” y si acertamos se nos dirá “La caja fuerte se ha abierto 
+        satisfactoriamente”. Tendremos cuatro oportunidades para abrir la caja 
+        fuerte.*/
+        
+        System.out.println("CONTROL ACCESO CAJA FUERTE");
+        System.out.println("--------------------------");
+    }
+}
