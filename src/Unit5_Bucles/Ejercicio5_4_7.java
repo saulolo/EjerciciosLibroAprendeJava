8a4b0de0a9bdf61c80f8614cca8cb52e0e6b1700
Saul Echeverri <saulolo@gmail.com>
2022-11-08 20:03:20 -0500

    Terminar el ejercicio del libro  Fecha: 08-11-22

--- src/Unit5_Bucles/Ejercicio5_4_7.java
+++ src/Unit5_Bucles/Ejercicio5_4_7.java
@@ -1,10 +1,14 @@
 package Unit5_Bucles;
 
+import java.util.Scanner;
+
 /* @author: Saulolo */
 
 public class Ejercicio5_4_7 {
 
     public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
         
         /*Realiza el control de acceso a una caja fuerte. La combinación será un
         número de 4 cifras. El programa nos pedirá la combinación para abrirla. 
@@ -15,5 +19,45 @@ public class Ejercicio5_4_7 {
         
         System.out.println("CONTROL ACCESO CAJA FUERTE");
         System.out.println("--------------------------");
+        
+        int clave = 1313;
+        int numero;
+        
+        
+//        for (int i = 0; i < 4; i++) {
+//            System.out.println("Ingresa la clave de la caja fuerte: ");
+//            numero = input.nextInt();
+//            if (numero == clave) {
+//                System.out.println("La caja fuerte se ha abierto satisfactoriamente.");
+//                break;
+//            } 
+//            
+//            else {
+//                System.out.println("Lo siento, esa no es la combinación.");
+//            }
+//
+//        }
+        
+        System.out.println("==========================");
+        System.out.println("SOLUCIÓN LIBRO");
+        
+        int intentos = 4;
+        int numeroIntroducido;
+        boolean acertado = false;
+        
+        do {    
+            System.out.println("Ingresa la clave de la caja fuerte: ");
+            numeroIntroducido = input.nextInt();
+            
+            if (numeroIntroducido == 1313) {
+                acertado = true;
+            } else {
+                System.out.println("Clave incorrecta.");
+            }
+            
+        } while ((intentos > 0) && (!acertado));
+        
+        
+
     }
 }
