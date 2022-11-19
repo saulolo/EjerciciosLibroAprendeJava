f69bfc885ea187a50b2f0eb45b0cbd8684449d22
Saul Echeverri <111350593+saulolo@users.noreply.github.com>
2022-11-18 19:25:51 -0500

    Ejercicios 16 Bucles. 18-11-22

---
+++ src/Unit5_Bucles/Ejercicio5_4_16.java
@@ -0,0 +1,18 @@
+package Unit5_Bucles;
+
+/* @author: Saulolo */
+
+public class Ejercicio5_4_16 {
+    
+    public static void main(String[] args) {
+        
+        /*Escribe un programa que diga si un número introducido por teclado es o
+        no primo. Un número primo es aquel que sólo es divisible entre él mismo 
+        y la unidad.*/
+        
+        System.out.println("NÚMERO PRIMO");
+        System.out.println("------------");
+        
+    }
+
+}
