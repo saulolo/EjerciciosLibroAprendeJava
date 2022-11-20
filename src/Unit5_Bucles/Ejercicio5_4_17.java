f73b4deb8b9781b1ff3d38776b532062c5825f19
Saul Echeverri <111350593+saulolo@users.noreply.github.com>
2022-11-20 18:16:26 -0500

    Ejercicio 17. Bucles. 20-11-22

---
+++ src/Unit5_Bucles/Ejercicio5_4_17.java
@@ -0,0 +1,20 @@
+package Unit5_Bucles;
+
+/* @author: Saulolo */
+
+public class Ejercicio5_4_17 {
+    
+    public static void main(String[] args) {
+        
+        /*Realiza un programa que sume los 100 números siguientes a un número 
+        entero y positivo introducido por teclado. Se debe comprobar que el dato 
+        introducido es correcto (que es un número positivo).*/
+        
+        System.out.println("SUMA DE NÚMEROS A UN ENTERO POSITIVO");
+        System.out.println("------------------------------------");
+        
+        
+
+    }
+
+}
