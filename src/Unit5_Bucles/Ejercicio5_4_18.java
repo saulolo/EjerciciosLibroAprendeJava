3a10b080330cade4a0d7f3d6331dac89901cfea1
Saul Echeverri <111350593+saulolo@users.noreply.github.com>
2022-11-22 12:15:04 -0500

    Ejercicio 18. Bucles. 20-11-22

---
+++ src/Unit5_Bucles/Ejercicio5_4_18.java
@@ -0,0 +1,18 @@
+package Unit5_Bucles;
+
+/* @author: Saulolo */
+
+public class Ejercicio5_4_18 {
+    
+    public static void main(String[] args) {
+        
+        /*Escribe un programa que obtenga los números enteros comprendidos entre
+        dos números introducidos por teclado y validados como distintos, el 
+        programa debe empezar por el menor de los enteros introducidos e ir 
+        incrementando de 7 en 7.*/
+        
+        System.out.println("RANGO ENTRE DOS NÚMEROS");
+        System.out.println("-----------------------");
+        
+    }
+}
