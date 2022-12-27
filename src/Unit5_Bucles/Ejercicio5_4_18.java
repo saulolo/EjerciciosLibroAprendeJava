d04b8f2ec00c8bde7c765479aa1b6146cd88e3c8
Saul Echeverri <111350593+saulolo@users.noreply.github.com>
2022-12-27 13:12:42 -0500

    Ejercicio 21. Bucles.

--- src/Unit5_Bucles/Ejercicio5_4_18.java
+++ src/Unit5_Bucles/Ejercicio5_4_18.java
@@ -1,10 +1,14 @@
 package Unit5_Bucles;
 
+import java.util.Scanner;
+
 /* @author: Saulolo */
 
 public class Ejercicio5_4_18 {
     
     public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
         
         /*Escribe un programa que obtenga los números enteros comprendidos entre
         dos números introducidos por teclado y validados como distintos, el 
@@ -14,5 +18,24 @@ public class Ejercicio5_4_18 {
         System.out.println("RANGO ENTRE DOS NÚMEROS");
         System.out.println("-----------------------");
         
+        int num1;
+        int num2;
+        int acum = 0;
+        
+        System.out.println("Introduce el primer número: ");
+        num1 = input.nextInt();
+        
+        System.out.println("Introduce el segundo número: ");
+        num2 = input.nextInt();
+        
+        if (num1 != num2) {
+            for (int i = num1; i < num2; i += 7) {
+                acum = acum + num1;
+                num1 = num1 + 7;
+                System.out.println(num1);  
+            }
+        } else {
+            System.out.println("Los números introducidos no pueden ser iguales.");
+        }  
     }
 }
