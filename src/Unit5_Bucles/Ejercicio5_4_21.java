2a2646a2c90bf48684fbc63d50d976e9d513ed7f
Saul Echeverri <111350593+saulolo@users.noreply.github.com>
2023-03-30 16:53:59 -0500

    Voy para el ejercicio 25

--- src/Unit5_Bucles/Ejercicio5_4_21.java
+++ src/Unit5_Bucles/Ejercicio5_4_21.java
@@ -1,45 +1,54 @@
 package Unit5_Bucles;
 
+import java.util.Scanner;
+
 /* @author: Saulolo */
-
 public class Ejercicio5_4_21 {
-    
-    public static void main(String[] args) {
-        
-        /*
-        Calcula la nota de un trimestre de la asignatura Programación. El programa 
-        pedirá las dos notas que ha sacado el alumno en los dos primeros controles. 
-        Si la media de los dos controles da un número mayor o igual a 5, el alumno
-        está aprobado y se mostrará la media. En caso de que la media sea un número
-        menor que 5, el alumno habrá tenido que hacer el examen de recuperación que
-        se califica como apto o no apto, por tanto se debe preguntar al usuario ¿Cuál
-        ha sido el resultado de la recuperación? (apto/no apto). Si el resultado
-        de la recuperación es apto, la nota será un 5; en caso contrario, se mantiene
-        la nota media anterior.
-        
-        Ejemplo 1:
-        Nota del primer control: 7
-        Nota del segundo control: 10
-        Tu nota de Programación es 8.5
-        
-        Ejemplo 2:
-        Nota del primer control: 6
-        Nota del segundo control: 3
-        ¿Cuál ha sido el resultado de la recuperación? (apto/no apto): apto
-        Tu nota de Programación es 5
-        
-        Ejemplo 3:
-        Nota del primer control: 6
-        Nota del segundo control: 3
-        ¿Cuál ha sido el resultado de la recuperación? (apto/no apto): no apto
-        Tu nota de Programación es 4.5
-        */
-        
-        System.out.println("CÁLCULO NOTA TRIMESTRE");
-        System.out.println("----------------------");
-        
-    }
-    
-    
 
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        /* Realiza un programa que vaya pidiendo números hasta que se introduzca 
+        un numero negativo y nos diga cuantos números se han introducido, la 
+        media de los impares y el mayor de los pares. El número negativo sólo se 
+        utiliza para indicar el final de la introducción de datos pero no se 
+        incluye en el cómputo.*/
+        
+        System.out.println("CONTADOR DE NÚMEROS");
+        System.out.println("-------------------");
+        
+        int numeroIntroducido;
+        int numeroDeElementos = 0;
+        int sumaImpares = 0;
+        int numeroDeElementosImpares = 0;
+        int maximoPar = 0;
+
+        System.out.println("Por favor, vaya introduciendo números enteros.");
+        
+        System.out.println("Puede terminar mediante la introducción de un número negativo.");
+        numeroIntroducido = input.nextInt();
+        
+
+        do {
+            numeroIntroducido = input.nextInt();
+
+            if (numeroIntroducido >= 0) {
+                numeroDeElementos++;
+                if ((numeroIntroducido % 2) == 1) { // número impar
+                    sumaImpares += numeroIntroducido;
+                    numeroDeElementosImpares++;
+                } else { // número par
+                    if (numeroIntroducido > maximoPar) {
+                        maximoPar = numeroIntroducido;
+                    }
+                }
+            }
+        } while (numeroIntroducido >= 0);
+
+        System.out.println("Ha introducido " + (numeroDeElementos - 1) + " números positivos.");
+        System.out.print("La media de los impares es ");
+        System.out.println(sumaImpares / numeroDeElementosImpares + ".");
+        System.out.println("El máximo de los pares es " + maximoPar + ".");
+    }
 }
---
+++ src/Unit5_Bucles/Ejercicio5_4_25.java
@@ -0,0 +1,23 @@
+package Unit5_Bucles;
+
+import java.util.Scanner;
+
+/* @author: Saulolo */
+public class Ejercicio5_4_25 {
+
+    public static void main(String[] args) {
+
+        /*Realiza un programa que pida un número por teclado y que luego muestre
+        ese número al revés.:
+         */
+        System.out.println("NÚMERO IMPRESO AL REVÉS");
+        System.out.println("-----------------------");
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Introduce un número: ");
+        int numero = input.nextInt();
+
+
+    }
+
+}
